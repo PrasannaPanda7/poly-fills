7509a7586a55a26a8487b93b7daf711c8dcf23d0
Tarinee476 <pandatarinee476@gmail.com>
2025-01-20 00:43:11 +0530

    useMemo polyfill added

--- Function/memoize.js
+++ Function/memoize.js
@@ -1,7 +1,26 @@
-function dummy() {
-  a = 10;
-  console.log(a);
+function useMemoCustom(callback) {
+  const map = new Map();
+  function memoizedCallBack(...args) {
+    const index = JSON.stringify(args);
+    if (map.has(index)) {
+      return map.get(index);
+    } else {
+      const res = callback(...args);
+      map.set(index, res);
+      return res;
+    }
+  }
+  return memoizedCallBack;
 }
-dummy();
-let a;
-a = 9;
+
+const sum = (...args) => {
+  console.log("called for arg: ", args);
+  return args.reduce((curr, sum) => curr + sum);
+};
+const memoizeSum = useMemoCustom(sum);
+
+console.log(memoizeSum(1, 2, 3));
+console.log(memoizeSum(1, 2, 5));
+console.log(memoizeSum(1, 2, 3));
+console.log(memoizeSum(1, 2, 4));
+console.log(memoizeSum(1, 2, 3));
